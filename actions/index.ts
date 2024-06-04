b6dc31b6d69793c1b346334ed0691b0f37872b99
outdrumz <barabanshikoff@gmail.com>
2024-06-04 23:49:04 +0400

    example

--- actions/index.ts
+++ actions/index.ts
@@ -1,23 +1,23 @@
 'use server';
 
-import { updateCoffeeStore } from "@/lib/airtable";
+import { updateCoffeeStore } from '@/lib/airtable';
 
 type State = {
-    id: string;
+  id: string;
 };
 
 export const upvoteAction = async (prevState: State) => {
-    console.log('upvoted');
+  console.log('upvote action');
 
-    const { id } = prevState;
+  const { id } = prevState;
 
-    const data = await updateCoffeeStore(id);
-    console.log({ data });
+  const data = await updateCoffeeStore(id);
+  console.log({ data });
 
-    if(data) {
-        return {
-            voting: data.length > 0 ? data[0].voting : 0,
-            id,
-        }
-    } 
+  if (data) {
+    return {
+      voting: data.length > 0 ? data[0].voting : 0,
+      id,
+    };
+  }
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,27 +1,26 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      
       backgroundImage: {
-        gradient: "url('/static/background.webp')",
+        gradient: "url('/static/background.png')",
       },
       colors: {
         purple: {
-         951: '#5755eb',
+          951: 'rgba(79, 70, 229, 1)',
         },
         gray: {
           950: 'hsla(0, 0%, 100%, 0.7);',
           951: 'hsla(0, 0%, 100%, 0.4);',
           952: '#373b64',
         },
-      }
+      },
     },
   },
   plugins: [],
